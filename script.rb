c5d2eeef9bc9156478b3e7281c0baad78242d70c
Omar Abd el Hameed <omarlabana@outlook.com>
2021-04-26 00:30:25 +0200

    Add pivot for testing

--- script.rb
+++ script.rb
@@ -1,21 +1,23 @@
 require_relative './lib/enumerable_module'
-
-# [1, 2, 3].my_each do |element|
-#   p element
-# end
-
-# (1..3).my_each do |element|
-#   p element
-# end
-
-# { 1 => 1, 2 => 2, 3 => 3 }.my_each do |element|
-#   p element
-# end
-
-# [1, 2, 3].my_each_with_index do |element, index|
-#   puts element, index
-# end
-
-[1, 2, 3].my_select do |element|
-  element != 3
+if false
+  [1, 2, 3].my_each do |element|
+    p element
+  end
+  
+  (1..3).my_each do |element|
+    p element
+  end
+  
+  { 1 => 1, 2 => 2, 3 => 3 }.my_each do |element|
+    p element
+  end
+  
+  [1, 2, 3].my_each_with_index do |element, index|
+    puts element, index
+  end
+  
+  [1, 2, 3].my_select do |element|
+    element != 3
+  end
 end
+  
