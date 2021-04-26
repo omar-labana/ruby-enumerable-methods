5e1fc5570145776fb29811c90674587c0f4107ad
Aduda-Boaz <adudaboaz@gmail.com>
2021-04-26 20:07:53 +0300

    Fix the rubocop identation errors

--- lib/enumerable_module.rb
+++ lib/enumerable_module.rb
@@ -1,12 +1,12 @@
 module Enumerable
   def my_each
     object_type = if instance_of?(Array)
-                    self
-                  elsif instance_of?(Range)
-                    to_a
-                  else
-                    flatten
-                  end
+      self
+    elsif instance_of?(Range)
+      to_a
+    else
+      flatten
+    end
     object_type.size.times do |index|
       yield object_type[index]
     end
@@ -14,12 +14,12 @@ module Enumerable
 
   def my_each_with_index
     object_type = if instance_of?(Array)
-                    self
-                  elsif instance_of?(Range)
-                    to_a
-                  else
-                    flatten
-                  end
+      self
+    elsif instance_of?(Range)
+      to_a
+    else
+      flatten
+    end
     object_type.size.times do |index|
       yield object_type[index], index
     end
@@ -46,24 +46,24 @@ module Enumerable
     true
   end
 
-    def my_any?
-        my_each { |element| return true if yield element }
-        false
-    end
+  def my_any?
+    my_each { |element| return true if yield element }
+    false
+  end
 
-    def my_count
-        count = 0
-        my_each { |element| count +=1 if yield element }
-        count
-    end
+  def my_count
+    count = 0
+    my_each { |element| count +=1 if yield element }
+    count
+  end
     
-    def my_map
-        pivot_array = []
-        my_each { |element| pivot_array << yield(element) }
-        p pivot_array
-    end
+  def my_map
+    pivot_array = []
+    my_each { |element| pivot_array << yield(element) }
+    p pivot_array
+  end
     
-    def my_inject inject_parameter = nil
+  def my_inject inject_parameter = nil
     if inject_parameter
       total = inject_parameter
       start_index = 0
@@ -74,7 +74,7 @@ module Enumerable
     self[start_index...self.length].my_each do |element|
       total = yield(total, element)
     end
-    total
-end
+      total
+  end
 
 end
