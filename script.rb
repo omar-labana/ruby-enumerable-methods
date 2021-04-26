3625b7d3e557c94a59011dd133cb1be4eb352188
Omar Abd el Hameed <omarlabana@outlook.com>
2021-04-26 20:09:28 +0200

    Fix linter errors

--- script.rb
+++ script.rb
@@ -1,42 +1,41 @@
 require_relative './lib/enumerable_module'
-if false
-  [1, 2, 3].my_each do |element|
-    p element
-  end
-
-  (1..3).my_each do |element|
-    p element
-  end
-
-  { 1 => 1, 2 => 2, 3 => 3 }.my_each do |element|
-    p element
-  end
-
-  [1, 2, 3].my_each_with_index do |element, index|
-    puts element, index
-  end
-
-  [1, 2, 3].my_select do |element|
-    element != 3
-  end
-
-  a = [1, 2, 3].my_all? do |element|
-    element > 4
-  end
-  a = [1, 2, 3].my_all? do |element|
-    element < 4
-  end
-  a = [1, 2, 3].my_any? do |element|
-    element == 5
-  end
-  a = [1, 2, 3].my_none? do |element|
-    element == 5
-  end
-  a = [1, 2, 3].my_count do |element|
-    element == 5
-  end
+[1, 2, 3].my_each do |element|
+  p element
 end
+
+(1..3).my_each do |element|
+  p element
+end
+
+{ 1 => 1, 2 => 2, 3 => 3 }.my_each do |element|
+  p element
+end
+
+[1, 2, 3].my_each_with_index do |element, index|
+  puts element, index
+end
+
+[1, 2, 3].my_select do |element|
+  element != 3
+end
+
+[1, 2, 3].my_all? do |element|
+  element > 4
+end
+[1, 2, 3].my_all? do |element|
+  element < 4
+end
+[1, 2, 3].my_any? do |element|
+  element == 5
+end
+[1, 2, 3].my_none? do |element|
+  element == 5
+end
+[1, 2, 3].my_count do |element|
+  element == 5
+end
+
 a = [1, 2, 3].my_map do |element|
   element * 2
 end
-p a
+puts a
