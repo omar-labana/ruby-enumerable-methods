50e62b7cbe3d57058913886b461fb1e13bcc4c68
panda4cs <panda4cs@gmail.com>
2021-05-10 22:11:22 +0200

    Add test for #my_select

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -4,16 +4,23 @@ require_relative '../lib/enumerable_module'
 array = [1, 2, 3, 4, 5]
 describe Enumerable do
   describe '#my_each' do
-    it 'maps an object to given block' do
+    it 'maps a block to a given object' do
       expect(array.my_each { |item| item += 1 })
         .to eql(array.each { |item| item += 1 })
     end
   end
 
   describe '#my_each_with_index' do
-    it 'maps each object element to given block along with its index' do
+    it 'maps a block to a given object along with each element index' do
       expect(array.my_each_with_index { |item, index| item += index })
         .to eql(array.each_with_index { |item, index| item += index })
+    end
+  end
+
+  describe '#my_select' do
+    it 'maps a block to a given object to select elements satisfying the block logic' do
+      expect(array.my_select{ |item| item > 1 })
+        .to eql(array.select { |item| item > 1 })
     end
   end
 end
